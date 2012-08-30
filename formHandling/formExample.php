58839ef170023a57c912da9c946b8e9deddd9d47
scott <131212224@polytechnicwest.wa.edu.au>
2012-08-30 09:21:41 +0800

    Form Examples

---
+++ formHandling/formExample.php
@@ -0,0 +1,35 @@
+
+<html>
+    <head>
+        
+        <meta http-equiv="content"
+     <title></title>   
+        
+    </head>
+    <body>
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+<form action="formExample.php" method="POST">
+    <label for="firstName">First Name</label>
+    <input id="firstName" type="text" name="firstName" value="" />
+    </br>
+    
+    <label for="firstName">Last Name</label>
+    <input id="lastName" type="text" name="lastName" value="" />
+    </br>
+    
+    <input type="submit" value="Submit" />
+    
+    
+    
+    
+    
+</form>
+</body>
+    </html
