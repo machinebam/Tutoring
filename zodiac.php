0a25820bdf0af137ad9944ccd640c419d7b2c7f6
scott <131212224@polytechnicwest.wa.edu.au>
2012-09-06 11:53:24 +0800

    zodiac soring using ksort (key sort) and krsort (reverse key sorting)

--- zodiac.php
+++ zodiac.php
@@ -9,22 +9,37 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-    
+
         <?php
-     $zodiacs = array("Aries"=>"20 March","Taurus"=>"20 April",
-                 "Gemini"=>"21 May","Cancer"=>"21 June",
-                 "Leo"=>"22 July","Virgo"=>"23 August",
-                 "Libra"=>"23 September","Scorpio"=>"23 October",
-                 "Sagittarius"=>"22 November","Capricorn"=>"22 December",
-                 "Aquarius"=>"20 January","Pisces"=>"20 March");
-    
-    print "Your Star Sign is " . $array [0] . "</br>\n";
-  
-    
-    
-    
-    
-    
+        $zodiacs = array("Aries" => "20 March", "Taurus" => "20 April",
+            "Gemini" => "21 May", "Cancer" => "21 June",
+            "Leo" => "22 July", "Virgo" => "23 August",
+            "Libra" => "23 September", "Scorpio" => "23 October",
+            "Sagittarius" => "22 November", "Capricorn" => "22 December",
+            "Aquarius" => "20 January", "Pisces" => "20 March");
+
+
+        print "<table>\n";
+        foreach ($zodiacs as $key => $value) {
+            print "<tr><td>$key</td><td>$value</td></tr>\n";
+        }
+        print '</table>';
+        print"<br/>";
+
+
+        ksort($zodiacs);
+        print "<table>\n";
+        foreach ($zodiacs as $key => $value) {
+            print "<tr><td>$key</td><td>$value</td></tr>\n";
+        }
+        print '</table>';
+        print"<br/>";
+        krsort($zodiacs);
+        print "<table>\n";
+        foreach ($zodiacs as $key => $value) {
+            print "<tr><td>$key</td><td>$value</td></tr>\n";
+        }
+        print '</table>';
         ?>
     </body>
 </html>
