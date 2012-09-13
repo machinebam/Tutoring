7679dd04b349a73a6f41c31d50c5b36373228a54
scott <131212224@polytechnicwest.wa.edu.au>
2012-09-13 12:18:18 +0800

    Database entries from client adding a CD

--- cdForm/cdDataBase.php
+++ cdForm/cdDataBase.php
@@ -10,44 +10,50 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        if(!empty($_POST)) {
-            
-            
-            print" Thank You. The album has been added</br> ";
-            
-                 $conn = mysqli_connect('localhost', 'root', '', 'cdcol', '3306');
-      if (!$conn) {
-          die('Could not connect to MySQL: ' . mysqli_connect_error());
-      }
-      mysqli_query($conn, 'SET NAMES \'utf8\'');
+        if (!empty($_POST)) {
 
-      echo '<table>';
-      echo '<tr>';
-      echo '<th>titel</th>';
-      echo '<th>interpret</th>';
-      echo '<th>jahr</th>';
-      echo '</tr>';
-      $result = mysqli_query($conn, 'SELECT titel, interpret, jahr FROM cds');
-      while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
-          echo '<tr>';
-          echo '<td>' . $row['titel'] . '</td>';
-          echo '<td>' . $row['interpret'] . '</td>';
-          echo '<td>' . $row['jahr'] . '</td>';
-          echo '</tr>';
-      }
-      mysqli_free_result($result);
-      echo '</table>';
-      
-      mysqli_close($conn);
-            
-        }
-        
-        else {
+            $conn = mysqli_connect('localhost', 'root', '', 'cdcol', '3306');
+            if (!$conn) {
+                die('Could not connect: ' . mysql_error());
+            }
+
+            mysql_select_db("cds", $conn);
+
+            mysql_query("INSERT INTO cds (Titel, interpret, jahr)
+VALUES ('Abbey Road', 'Beatles',1970)");
+
+
+            mysql_close($conn);
+            print" Thank You. The album has been added</br> ";
+
+            $conn = mysqli_connect('localhost', 'root', '', 'cdcol', '3306');
+            if (!$conn) {
+                die('Could not connect to MySQL: ' . mysqli_connect_error());
+            }
+            mysqli_query($conn, 'SET NAMES \'utf8\'');
+
+            echo '<table>';
+            echo '<tr>';
+            echo '<th>titel</th>';
+            echo '<th>interpret</th>';
+            echo '<th>jahr</th>';
+            echo '</tr>';
+            $result = mysqli_query($conn, 'SELECT titel, interpret, jahr FROM cds');
+            while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
+                echo '<tr>';
+                echo '<td>' . $row['titel'] . '</td>';
+                echo '<td>' . $row['interpret'] . '</td>';
+                echo '<td>' . $row['jahr'] . '</td>';
+                echo '</tr>';
+            }
+            mysqli_free_result($result);
+            echo '</table>';
+
+            mysqli_close($conn);
+        } else {
             print "You need to go <a href=\"http://localhost/ScottDip/cdForm/cdForm.php\">
                 here</a>";
         }
-        
-        
         ?>
     </body>
 </html>
