d6640df913ce3b808d5e2481fecfd5611920f4b3
scott <131212224@polytechnicwest.wa.edu.au>
2012-10-11 09:50:56 +0800

    Wishlist Tutorial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form name="wishList" action="wishlist.php">
+            Show wishlist of: <input type="text" name="user" value="" />
+            <input type="submit" value="Go" />
+        </form>
+  
+</form>
+        
+        
+    </body>
+</html>
