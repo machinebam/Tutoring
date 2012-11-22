89150dcde7887e7eb03b633a2f61223d0b112587
scott <131212224@polytechnicwest.wa.edu.au>
2012-11-22 11:52:51 +0800

    Search Results on index page

---
+++ Shopping_Cart/searchResults.php
@@ -0,0 +1,17 @@
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Seach Results</title>
+    </head>
+    <body>
+        <h1>Search Results</h1>
+        
+        
+        <!-- google code-->
+    </body>
+</html>
