74f6c401947aad59ea74c8331ae1675ff659390a
scott <131212224@polytechnicwest.wa.edu.au>
2012-11-22 12:10:55 +0800

    Search Results on index page

--- Shopping_Cart/searchResults.php
+++ Shopping_Cart/searchResults.php
@@ -10,8 +10,58 @@ and open the template in the editor.
     </head>
     <body>
         <h1>Search Results</h1>
+
+
+        <?php
+        if (array_key_exists('q', $_GET)) {
+
+
+            $conn = mysqli_connect('localhost', 'root', '', 'cheese_shop', '3306');
+            if (!$conn) {
+                die('Could not connect to MySQL: ' . mysqli_connect_error());
+            }
+            mysqli_query($conn, 'SET NAMES \'utf8\'');
+
+            echo '<table>';
+            echo '<tr>';
+            echo '<th>item_name</th>';
+            echo '<th>unit_price</th>';
+            echo '<th> </th>';
+            echo '</tr>';
+      $query = "SELECT item_id, item_name, unit_price, picture_file_name
+                FROM items
+                    WHERE item_name
+                        LIKE '%  %'
         
-        
+                OR item_description
+                        LIKE '%   %'
+
+                    ";
+
+            $result = mysqli_query($conn, $query);
+
+
+            while (($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) != NULL) {
+                echo '<tr>';
+
+                echo '<td>' . $row['item_name'] . '</td>';
+
+
+                echo '<td>' . $row['unit_price'] . '</td>';
+                echo '<td>' . $row['picture_file_name'] . '</td>';
+                echo '</tr>';
+            }
+        } else {
+
+            print "No results available";
+        }
+        ?>
+
+
+
+
+
+
         <!-- google code-->
     </body>
 </html>
